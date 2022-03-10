35afd931fb7d42bd29c93dca81860333cfe5468b
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-03-10 09:17:41 -0800

    feat: convert app to typescript

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [holds, setHolds] = useState([]);
+
+  const handleGetHolds = async () => {
+    fetch("/api/holds", { method: "GET" })
+      .then((res) => res.json())
+      .then(setHolds);
+  };
+
+  return (
+    <div>
+      <h1>Skyrim API</h1>
+
+      <button onClick={handleGetHolds}>Get Holds</button>
+
+      {holds &&
+        holds.map(
+          (hold: {
+            name: string;
+            capital: string;
+            jarl: string;
+            allegiance: string;
+          }) => {
+            return (
+              <div>
+                <h3>{hold.name}</h3>
+                <p>Capital: {hold.capital}</p>
+                <p>Jarl: {hold.jarl}</p>
+                <p>Allegiance: {hold.allegiance}</p>
+              </div>
+            );
+          }
+        )}
+    </div>
+  );
+};
+
+export default App;
